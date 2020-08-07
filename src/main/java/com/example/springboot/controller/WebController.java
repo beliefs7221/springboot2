d4785901b5f8bd63b7fdc497fa4b61d5ce79fe3a
beliefs <beliefs@163.com>
2020-08-07 22:24:37 +0800

    wangaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/main/java/com/example/springboot/controller/WebController.java
+++ src/main/java/com/example/springboot/controller/WebController.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
 public class WebController {
 
-    @Autowired
+    @Resource
     private UserService userService;
 
     @RequestMapping("/web")
