c7adc694f6d8f30393c1015b8a18413cfc16225a
beliefs <beliefs@163.com>
2020-09-28 11:22:04 +0800

    wangaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -20,7 +20,7 @@ class SpringbootApplicationTests {
 
     void f(List list){
     	String s="askhd";
-        int i = 5;
+        int i = 7;
         list.add(1);
         list.add(3);
         list.add(2);
